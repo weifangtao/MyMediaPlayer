db777104edd259a7a02b06f8c9b4ed579eac3ae4
zhangsan <111>
2017-01-15 19:11:02 +0800

    初始化项目的完成

---
+++ app/src/main/java/com/itheima/mymediaplayer/ui/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.itheima.mymediaplayer.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * 类    名:  BaseActivity
+ * 创 建 者:  魏方涛
+ * 创建时间:  2017/1/15 18:40
+ * 更新者  :  $Author$
+ * 更新时间:  $Date$
+ * 描    述:  ${TODO}
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        ButterKnife.bind(this);
+        init();
+    }
+
+    /**
+     * 初始化的时候使用,选择性继承
+     */
+    protected void init() {
+    }
+
+    /**
+     * 初始化,获取布局文件
+     * @return
+     */
+    protected abstract int getLayoutResId();
+}
---
+++ app/src/main/java/com/itheima/mymediaplayer/ui/BaseFragment.java
@@ -0,0 +1,40 @@
+package com.itheima.mymediaplayer.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * 类    名:  BaseFragment
+ * 创 建 者:  魏方涛
+ * 创建时间:  2017/1/15 19:06
+ * 更新者  :  $Author$
+ * 更新时间:  $Date$
+ * 描    述:  ${TODO}
+ */
+
+public abstract class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(getLayoutResId(), null);
+        ButterKnife.bind(rootView);
+        init();
+        return rootView;
+    }
+    /**
+     * 初始化数据
+     */
+    protected abstract void init();
+    /**
+     * 舒适化布局
+     * @return
+     */
+    protected abstract int getLayoutResId();
+}
